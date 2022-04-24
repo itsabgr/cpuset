d86725cdaf0fd8c5d701b846dd8ae2da5eb8524e
abgr <tbpmtbpm@gmail.com>
2022-04-24 15:47:53 +0430

    init

---
+++ cpuset.go
@@ -0,0 +1,39 @@
+package cpuset
+
+import (
+	"golang.org/x/sys/unix"
+	"runtime"
+)
+
+func CPUGet(tid int) []int {
+	if tid < 0 {
+		tid = unix.Gettid()
+	}
+	set := &unix.CPUSet{}
+	set.Zero()
+	err := unix.SchedGetaffinity(tid, set)
+	handy.Throw(err)
+	result := make([]int, 0, runtime.NumCPU())
+	for i := range handy.N(uint(runtime.NumCPU())) {
+		if set.IsSet(i) {
+			result = append(result, i)
+		}
+	}
+	return result
+}
+func CPUSet(tid int, ns ...int) error {
+	if tid < 0 {
+		tid = unix.Gettid()
+	}
+	set := &unix.CPUSet{}
+	set.Zero()
+	for _, n := range ns {
+		set.Set(n)
+	}
+	return unix.SchedSetaffinity(tid, set)
+}
+func Nice(tid int, nice int) error {
+	if tid < 0 {
+		tid = unix.Gettid()
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/itsabgr/cputset
+
+go 1.18
+
+require golang.org/x/sys v0.0.0-20220422013727-9388b58f7150
